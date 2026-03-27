7b6f8afb67478dbf290ae8cec47aff36ab9111d5
Pratik Rai <raipratik0101@gmail.com>
2026-03-28 01:14:44 +0530

    fix(apollo): explicitly use HttpLink for apollo client v4 compatibility and fix imports

--- frontend/src/services/GraphAPI.js
+++ frontend/src/services/GraphAPI.js
@@ -1,7 +1,7 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
+import { ApolloClient, InMemoryCache, HttpLink, gql } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'http://localhost:8000/graphql',
+  link: new HttpLink({ uri: 'http://localhost:8000/graphql' }),
   cache: new InMemoryCache(),
 });
 
